97a429d9aae5e0868afb712fea35c5fa681a87c6
Pat Bates <pat.bates@dbtlabs.com>
2026-03-04 10:06:44 -0500

    Auto commit for Studio handoff

---
+++ models/canvas/cust_fact.sql
@@ -0,0 +1,22 @@
+WITH dim_customers AS (
+  SELECT
+    *
+  FROM {{ ref('pb_jaffle_shop', 'dim_customers') }}
+), fct_orders AS (
+  SELECT
+    *
+  FROM {{ ref('pb_jaffle_shop', 'fct_orders') }}
+), join_1 AS (
+  SELECT
+    *
+  FROM dim_customers
+  JOIN fct_orders
+    USING (CUSTOMER_ID)
+), cust_fact_sql AS (
+  SELECT
+    *
+  FROM join_1
+)
+SELECT
+  *
+FROM cust_fact_sql
