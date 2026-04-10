ac34156e101387a7af7aaf29e2b7f1870e6ba3a7
patbates-sa <pat.bates@dbtlabs.com>
2026-04-10 06:02:29 -0400

    added mcp server and claude code changes

--- models/marts/new_object.sql
+++ models/marts/new_object.sql
@@ -1,4 +1,5 @@
 SELECT
-    *
-  FROM {{ ref('pb_jaffle_shop', 'dim_customers') }}
-  
+    customer_id,
+    first_name || ' ' || last_name AS customer_name,
+    number_of_orders
+FROM {{ ref('pb_jaffle_shop', 'dim_customers') }}
