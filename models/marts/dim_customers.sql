463549d6b63d9a06e30e851281d5352ecea77fed
Pat Bates <pat.bates@dbtlabs.com>
2026-04-17 11:47:27 +0000

    new changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -20,6 +20,18 @@ with
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
             coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+            case
+                when customer_orders.first_order_date is null then 0
+                else customer_orders.number_of_orders
+                / greatest(
+                    datediff(
+                        'month',
+                        customer_orders.first_order_date,
+                        customer_orders.most_recent_order_date
+                    ) + 1,
+                    1
+                )
+            end as average_monthly_orders,
             customer_orders.lifetime_value,
         from customers
         left join customer_orders using (customer_id)
