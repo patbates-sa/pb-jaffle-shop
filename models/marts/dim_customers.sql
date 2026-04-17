32896f0506ad8782cfd188fb3ff41a271f180d79
Pat Bates <pat.bates@dbtlabs.com>
2026-04-17 10:24:39 +0000

    remove demo changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -17,21 +17,10 @@ with
             customers.customer_id,
             customers.first_name,
             customers.last_name,
-            customers.last_name as given_name,
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
             coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
             customer_orders.lifetime_value,
-            coalesce(customer_orders.number_of_orders, 0)
-            / nullif(
-                datediff(
-                    month,
-                    customer_orders.first_order_date,
-                    customer_orders.most_recent_order_date
-                )
-                + 1,
-                0
-            ) as avg_monthly_orders
         from customers
         left join customer_orders using (customer_id)
     )
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,6 +1,5 @@
 select
         id as order_id,
-        id as new_order_id,
         user_id as customer_id,
         order_date,
         status as order_status
