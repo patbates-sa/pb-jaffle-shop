4f7323887199eedbb5b6c851b80415e0d34b3137
Pat Bates <pat.bates@dbtlabs.com>
2026-05-03 20:46:28 +0000

    demo changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -20,7 +20,14 @@ with
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
             cast(coalesce(customer_orders.number_of_orders, 0) as number(18,0)) as number_of_orders,
-            cast(customer_orders.lifetime_value as number(38,6)) as lifetime_value
+            cast(customer_orders.lifetime_value as number(38,6)) as lifetime_value,
+            cast(
+                case
+                    when customer_orders.first_order_date is null then 0
+                    else coalesce(customer_orders.number_of_orders, 0)
+                        / greatest(datediff('month', customer_orders.first_order_date, customer_orders.most_recent_order_date) + 1, 1)
+                end as number(38,6)
+            ) as average_monthly_orders
         from customers
         left join customer_orders using (customer_id)
     )
@@ -31,5 +38,6 @@ select
     first_order_date,
     most_recent_order_date,
     number_of_orders,
-    lifetime_value
+    lifetime_value,
+    average_monthly_orders
 from final
