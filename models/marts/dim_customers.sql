ec7b3cc0b5f7d54e0bbdfc51af60f1f8915a238c
patbates-sa <pat.bates@dbtlabs.com>
2026-05-18 13:25:15 -0400

    KAN-14: roll back KAN-12 and KAN-13 changes

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -20,15 +20,7 @@ with
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
             cast(coalesce(customer_orders.number_of_orders, 0) as number(18,0)) as number_of_orders,
-            cast(customer_orders.lifetime_value as number(38,6)) as lifetime_value,
-            -- KAN-12: average monthly orders = orders divided by months spanned between first and most recent order
-            cast(
-                coalesce(customer_orders.number_of_orders, 0)
-                / greatest(
-                    datediff('month', customer_orders.first_order_date, customer_orders.most_recent_order_date) + 1,
-                    1
-                ) as number(38,6)
-            ) as avg_monthly_orders
+            cast(customer_orders.lifetime_value as number(38,6)) as lifetime_value
         from customers
         left join customer_orders using (customer_id)
     )
@@ -39,6 +31,5 @@ select
     first_order_date,
     most_recent_order_date,
     number_of_orders,
-    lifetime_value,
-    avg_monthly_orders
+    lifetime_value
 from final
